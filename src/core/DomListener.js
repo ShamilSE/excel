ed60c9db79e8a7660f0a0511855fdb34f6b79842
Shamil <ishamil86@gmail.com>
2020-10-08 16:49:42 +0500

    listeners structure

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,15 @@
 export class DomListener {
-    // constructor($root) {
-    //     // if (!$root) {
-    //     //     throw Error('No $root element provided')
-    //     // }
-    //     // this.$root = $root
-    // }
+    constructor($root, listeners = []) {
+        if (!$root) {
+            throw Error('No $root element provided')
+        }
+        this.$root = $root
+        this.listeners = listeners
+    }
+
+    initDomListeners() {
+        console.log(this.listeners)
+    }
+
+    removeDomListeners() {}
 }
