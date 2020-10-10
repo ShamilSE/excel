b42cd3365bf5e280fea9f7d0841ceda46067098a
Shamil <ishamil86@gmail.com>
2020-10-11 00:19:25 +0500

    resize for header

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 import {createTable} from "@/components/table/table.template";
+import {$} from "@core/dom";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -8,13 +9,23 @@ export class Table extends ExcelComponent {
     constructor($root) {
         super($root, {
             name: 'Table',
-            listeners: []
+            listeners: ['mousedown']
         })
     }
 
-
-
     toHTML() {
         return createTable(40)
+    }
+
+    onMousedown(event) {
+        if (event.target.dataset.resize) {
+            const $target = $(event.target)
+            const $parent = $target.closest('[data-type="resizeble"]')
+            const coords = $parent.getCoords()
+            document.onmousemove = e => {
+                const delta = e.pageX - coords.right
+                $parent.$el.style.width = coords.width + delta + 'px'
+            }
+        }
     }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -12,19 +12,20 @@ function createCell() {
 function createColumn(columns) {
     return `
 
-        <div class="column">
+        <div class="column" data-type="resizeble">
             ${columns}
-            <div class="col-resize"></div>
+            <div class="col-resize" data-resize="column"></div>
         </div>
     `
 }
 
 function createRow(content, index = '') {
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
     return `
         <div class="row">
             <div class="row-info">
                 ${index}
-                <div class="row-resize"></div>
+                ${resize}
             </div>
             <div class="row-data">${content}</div>
         </div>
@@ -39,7 +40,7 @@ export function createTable(rowsCount = 5) {
     for (let i = CODES.A; i < CODES.Z +1; i++) {
         columns.push(createColumn(String.fromCharCode(i)))
     }
-    rows.push(createRow(columns.join('').trim()))
+    rows.push(createRow(columns.join('').trim()), null)
     // наполнение последующих строк
     for (let i = 0; i < rowsCount; i++) {
         columns = []
