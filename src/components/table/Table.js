aea01a25d8d60f8df5a0e9e8a5979d1bc2039dfb
Shamil <ishamil86@gmail.com>
2020-10-11 15:18:21 +0500

    resize all the column

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -22,10 +22,12 @@ export class Table extends ExcelComponent {
             const $target = $(event.target)
             const $parent = $target.closest('[data-type="resizeble"]')
             const coords = $parent.getCoords()
+            const cells = this.$root.findAll(`[data-column-index="${$parent.data.columnIndex}"]`)
 
             document.onmousemove = e => {
                 const delta = e.pageX - coords.right
                 $parent.$el.style.width = coords.width + delta + 'px'
+                cells.forEach(el => el.style.width = coords.width + delta + 'px')
             }
 
             document.onmouseup = () => {
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,16 +3,15 @@ const CODES = {
     Z: 90,
 }
 
-function createCell() {
+function createCell(index) {
     return `
-        <div class="cell" contenteditable=""></div>
+        <div class="cell" contenteditable="" data-column-index="${index}"></div>
     `
 }
 
-function createColumn(columns) {
+function createColumn(columns, index) {
     return `
-
-        <div class="column" data-type="resizeble">
+        <div class="column" data-type="resizeble" data-column-index="${index}">
             ${columns}
             <div class="col-resize" data-resize="column"></div>
         </div>
@@ -33,19 +32,18 @@ function createRow(content, index = '') {
 }
 
 export function createTable(rowsCount = 5) {
-    const colsCount = CODES.Z - CODES.A + 1
     const rows = []
     let columns = []
     // наполнение первой строки
     for (let i = CODES.A; i < CODES.Z +1; i++) {
-        columns.push(createColumn(String.fromCharCode(i)))
+        columns.push(createColumn(String.fromCharCode(i), i))
     }
     rows.push(createRow(columns.join('').trim()), null)
     // наполнение последующих строк
     for (let i = 0; i < rowsCount; i++) {
         columns = []
-        for (let i = 0; i < colsCount; i++) {
-            columns.push(createCell())
+        for (let i = CODES.A; i < CODES.Z +1; i++) {
+            columns.push(createCell(i))
         }
         rows.push(createRow(columns.join(''), i + 1))
     }
