864796eb14d7a8765ad81c84dc725bbf29ae7f1e
Shamil <ishamil86@gmail.com>
2020-10-11 20:23:49 +0500

    resolving merge conflicts part2

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 import {createTable} from "@/components/table/table.template";
+import {resizer} from "@/components/table/ resizer";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -17,27 +18,7 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            const $target = $(event.target)
-            const $parent = $target.closest('[data-type="resizeble"]')
-            const coords = $parent.getCoords()
-            const cells = this.$root.findAll(`[data-column-index="${$parent.data.columnIndex}"]`)
-
-            document.onmousemove = e => {
-                if (event.target.dataset.resize == 'column') {
-                    const delta = e.pageX - coords.right
-                    const value = coords.width + delta + 'px'
-                    $parent.$el.style.width = value
-                    cells.forEach(el => el.style.width = value)
-                } else {
-                    const delta = e.pageY - coords.bottom
-                    const value = coords.height + delta + 'px'
-                    $parent.$el.style.height = value
-                }
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null
-            }
+            resizer(this.$root, event)
         }
     }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -55,6 +55,12 @@ class Dom {
     getCoords() {
         return this.$el.getBoundingClientRect()
     }
+
+    css(styles = {}) {
+        Object.keys(styles).forEach(key => {
+            this.$el.style[key] = styles[key]
+        })
+    }
 }
 
 export function $(selector) {
